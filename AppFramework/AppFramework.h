fb008a3488a67c92153659043ece35ad68822e02
Levi Bostian <levi.bostian@gmail.com>
2018-10-24 11:56:11 -0500

    Create Playground with SnapKit and RxSwift for view prototyping.

---
+++ AppFramework/AppFramework.h
@@ -0,0 +1,19 @@
+//
+//  AppFramework.h
+//  AppFramework
+//
+//  Created by Levi Bostian on 10/24/18.
+//  Copyright © 2018 Levi Bostian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppFramework.
+FOUNDATION_EXPORT double AppFrameworkVersionNumber;
+
+//! Project version string for AppFramework.
+FOUNDATION_EXPORT const unsigned char AppFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFramework/PublicHeader.h>
+
+
---
+++ iOSViewsPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSViewsPlayground
+//
+//  Created by Levi Bostian on 10/24/18.
+//  Copyright © 2018 Levi Bostian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
